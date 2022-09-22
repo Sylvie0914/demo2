bb1da0828e7bd4e16768fab43e67cff70435069b
Sylvie0914 <112403178+Sylvie0914@users.noreply.github.com>
2022-09-22 06:16:10 -0400

    Modifying 2 files

--- b575hw12b_script.sh
+++ b575hw12b_script.sh
@@ -7,3 +7,4 @@ sed 's/AGATCGGAAGAGC.*//' sample_rseq.txt > sample_rseq_CUT.txt
 echo "#== CUT ADAPTER DONE"
 diff sample_rseq_CUT.txt sample_cutadapt_rseq.txt
 echo "#== SCRIPT DONE"
+The last time the script was ran is Thu Sep 22 06:11:37     2022
